ded1b795fcb8fb2192f01767f0983209c1888d5b
no-blyat <psudarshan37@gmai.com>
2020-01-23 23:20:37 +0530

    Login testing

--- RMA/urls.py
+++ RMA/urls.py
@@ -21,6 +21,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
-    path('login/', auth_views.login,{'template_name': '/login.html'}, name='signin'),
-    path('logout/', auth_views.logout, name='signout'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name=''), name='signout'),
 ]
--- users/README.md
+++
@@ -1,4 +0,0 @@
-# HEADS-UP
-
-A web-app which lets the user keep track the price of selected product in amazon.in.
-Email will be sent when the price drops below the required amount provided by user.
