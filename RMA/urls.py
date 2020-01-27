a7c83bbbba1657dd047d09c4ed4403b38cf16044
no-blyat <psudarshan37@gmai.com>
2020-01-27 20:02:02 +0530

    Login Required

--- RMA/urls.py
+++ RMA/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='main/home.html'), name='signout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('home/', main_views.home, name='home'),
     path('confirm/', main_views.confirm, name='confirm'),
     path('', main_views.home, name='home'),
--- main/views.py
+++ main/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .forms import QueryForm
 from .scrape import ScrapeAmzn
 # Create your views here.
 
+@login_required
 def home(request):
     form = QueryForm()
     return render(request, 'main/home.html', {'form': form})
 
+@login_required
 def confirm(request):
     if request.method == 'POST':
         form = QueryForm(request.POST)
